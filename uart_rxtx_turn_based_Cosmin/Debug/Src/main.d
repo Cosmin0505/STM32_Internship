4b110fb41bacd8906c4d9c19a02b710bc2cfe258
Busila <mihai-cosmin.busila@bertrandt.com>
2026-08-03 13:09:58 +0300

    Cosmin - UART RXTX Turn Based
    Signed-off-by: Busila <mihai-cosmin.busila@bertrandt.com>

---
+++ uart_rxtx_turn_based_Cosmin/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/busilam/Cosmin/Traingings/STM32_Bare_Metal/STM32Cube_FW_F0_V1.10.0/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f070xb.h \
+ C:/Users/busilam/Cosmin/Traingings/STM32_Bare_Metal/STM32Cube_FW_F0_V1.10.0/Drivers/CMSIS/Include/core_cm0.h \
+ C:/Users/busilam/Cosmin/Traingings/STM32_Bare_Metal/STM32Cube_FW_F0_V1.10.0/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/busilam/Cosmin/Traingings/STM32_Bare_Metal/STM32Cube_FW_F0_V1.10.0/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/busilam/Cosmin/Traingings/STM32_Bare_Metal/STM32Cube_FW_F0_V1.10.0/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/busilam/Cosmin/Traingings/STM32_Bare_Metal/STM32Cube_FW_F0_V1.10.0/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ ../Inc/uart.h
+C:/Users/busilam/Cosmin/Traingings/STM32_Bare_Metal/STM32Cube_FW_F0_V1.10.0/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f070xb.h:
+C:/Users/busilam/Cosmin/Traingings/STM32_Bare_Metal/STM32Cube_FW_F0_V1.10.0/Drivers/CMSIS/Include/core_cm0.h:
+C:/Users/busilam/Cosmin/Traingings/STM32_Bare_Metal/STM32Cube_FW_F0_V1.10.0/Drivers/CMSIS/Include/core_cmInstr.h:
+C:/Users/busilam/Cosmin/Traingings/STM32_Bare_Metal/STM32Cube_FW_F0_V1.10.0/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/busilam/Cosmin/Traingings/STM32_Bare_Metal/STM32Cube_FW_F0_V1.10.0/Drivers/CMSIS/Include/core_cmFunc.h:
+C:/Users/busilam/Cosmin/Traingings/STM32_Bare_Metal/STM32Cube_FW_F0_V1.10.0/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+../Inc/uart.h:
